aee50ae86ac95139cbebf6d914e1a4ef56f6b538
bing0930 <60236802+bing0930@users.noreply.github.com>
2023-04-27 12:06:41 +0800

    main

---
+++ product.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JIA Modern Lighting House</title>
+    <link rel="icon" type="image/x-icon" href="image/favicon.ico">
+    <link rel="stylesheet" href="assets/css/productStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <?php include "header.php"; ?>
+
+    <main>
+
+    <div class="container mt-100">                   
+        <div class="row">
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="surface.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/surface/surfaceProduct.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/surface/sc1.png" alt="Category"><img src="image/dropdown/surface/sc2.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Surface Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="surface.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="recessive.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/recessive/r3.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/recessive/r1.png" alt="Category"><img src="image/dropdown/recessive/r2.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Recessive Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="recessive.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="wallLight.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/wallLight/w1.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/wallLight/w2.png" alt="Category"><img src="image/dropdown/wallLight/w3.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Wall Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="wallLight.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="pendant.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/pendant/p1.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/pendant/p5.png" alt="Category"><img src="image/dropdown/pendant/p12.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Pendant Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="pendant.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="step.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/step/s3.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/step/s2.png" alt="Category"><img src="image/dropdown/step/s1.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Step Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="step.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="outdoorL.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/outdoorL/ol1.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/outdoorL/ol5.png" alt="Category"><img src="image/dropdown/outdoorL/ol12.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Outdoor Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="outdoorL.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="outdoorW.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/outdoorW/ow1.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/outdoorW/ow5.png" alt="Category"><img src="image/dropdown/outdoorW/ow10.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Outdoor Wall Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="outdoorW.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="outdoorP.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/outdoorP/op3.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/outdoorP/op1.png" alt="Category"><img src="image/dropdown/outdoorP/op2.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Outdoor Pendant Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="outdoorP.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="table.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/table/t1.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/table/t2.png" alt="Category"><img src="image/dropdown/table/t3.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Table Lamp</h4>
+                <a class="btn btn-outline-danger btn-sm" href="table.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="track.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/track/tr2.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/track/tr1.png" alt="Category"><img src="image/dropdown/track/tr4.png" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Track Light</h4>
+                <a class="btn btn-outline-danger btn-sm" href="track.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="others.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/others/o6.jpg" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/others/o7.jpg" alt="Category"><img src="image/dropdown/others/o8.jpg" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Others</h4>
+                <a class="btn btn-outline-danger btn-sm" href="others.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="card mb-30"><a class="card-img-tiles" href="fans.php" data-abc="true">
+                <div class="inner">
+                  <div class="main-img"><img src="image/dropdown/fan/f1.png" alt="Category"></div>
+                  <div class="thumblist"><img src="image/dropdown/fan/f2.jpg" alt="Category"><img src="image/dropdown/fan/f3.jpg" alt="Category"></div>
+                </div></a>
+              <div class="card-body text-center">
+                <h4 class="card-title">Fans</h4>
+                <a class="btn btn-outline-danger btn-sm" href="wallLight.php" data-abc="true">View Products</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
+
+</main>
+<?php include "footer.php"; ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
